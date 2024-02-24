9243f64230c1307421d282a408037af324280ad3
Eric Alexander <eric@ericalexander.org>
2024-02-24 16:48:53 +0000

    update

--- create_react_json.py
+++ create_react_json.py
@@ -1,49 +1,23 @@
 import json
 import os
 
-def create_file_structure_dict(startpath):
-    data = {}
-
-    for root, dirs, files in os.walk(startpath):
-        for dir in dirs:
-            if dir not in data:
-                data[dir] = {}
-        for name in files:
-            parents = root.split("/")
-            parents.pop(0) # remove the first directory json
-            #print(parents)
-            #k = data
-            for p in parents:
-                print(p)
-                if p not in data:
-                    print("not in")
-                    print(p)
-                    #k[p] = {}
-                    #k = k[p]
-
-            file_key = name.split(".")[0]
-            full_path = os.path.join(root, name)
-            #parent_dir = os.path.basename(root)
-            #if parent_dir not in data:
-            #    data[parent_dir] = {}
-            f = open(full_path)
-            #k[file_key] = json.load(f)
-
-            #print(root)
-            #parents = root.split("/")
-            #print(parents)
-            #k = data
-            #for p in parents:
-            #    if p != "json":
-            #        if p not in k:
-            #            k[p] = {}
-            #            k = k[p]
-
-    return data
-
+def dir_to_dict(directory):
+    directory_structure = {}
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            directory_structure[item] = dir_to_dict(item_path)
+        elif item.endswith('.json'):
+            with open(item_path, 'r') as json_file:
+                data = json.load(json_file)
+            key = item.split('.')[0]
+            directory_structure[key] = data
+        else:
+            directory_structure[item] = None
+    return directory_structure
 
 def save_file_structure_to_json(startpath, output_file):
-    file_structure_dict = create_file_structure_dict(startpath)
+    file_structure_dict = dir_to_dict(startpath)
     with open(output_file, 'w') as json_file:
         json.dump(file_structure_dict, json_file, indent=4)
 
